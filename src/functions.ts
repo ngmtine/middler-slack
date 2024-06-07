837509da5f85da8b0caf2b6a07c3949e5a5bc73a
ngmtine <ngmfries@gmail.com>
2024-06-07 19:01:26 +0900

    update: ループカウンタ追加して応答ない場合は強制終了

--- src/functions.ts
+++ src/functions.ts
@@ -22,33 +22,51 @@ export const startBrowser = async (url: string): Promise<Page> => {
     return chatgptPage;
 };
 
+let prevConversationTurn = "";
+
 // chatgptの回答を待つ
 export const chatgptMonitoring = async ({ page }: { page: Page }): Promise<string> => {
-    let generatingText = "";
+    console.log(prevConversationTurn);
 
-    const interval = env.waitingInterval; // 短すぎると回答完了前にreturnしてしまう事に注意
+    let generatingText = "";
+    const interval = env.waitingInterval;
     const timer = promiseSetInterval(interval);
+
+    let loopCounter = 0;
 
     for await (const _ of timer) {
         try {
+            // 応答がない場合強制終了
+            loopCounter++;
+            if (loopCounter > 100) {
+                generatingText = "timeout!! No response from chatgpt!!";
+                break;
+            }
+
             // ヘッドレスで起動している場合は不要
             page.bringToFront();
 
             // 最後の回答の要素を取得
-            const messageContentList = await page.$$("div.markdown");
+            const messageContentList = await page.$$("div[data-testid]");
             const lastMessageSection = messageContentList.at(-1);
             if (!lastMessageSection) continue;
 
-            // 最後の回答の要素のinnerHtmlを取得
-            const htmlText = await lastMessageSection.evaluate((div) => div.innerHTML);
+            // 最後の回答の要素の平文htmlを取得
+            const htmlText = await lastMessageSection.evaluate((div) => div.outerHTML);
 
             // HTMLDivElementに変換
             const doc = text2HTMLDocument(htmlText);
             const div = doc.getElementsByTagName("div")[0];
-            if (!div) throw new Error("cannot get div!!");
+            if (!div) continue;
 
-            // markdownテキストに変換
-            const text = html2markdown(div);
+            // 前回の回答を取得した場合はループ継続
+            const conversationTurn = div.dataset.testid ?? "";
+            if (prevConversationTurn === conversationTurn) continue;
+
+            // markdownテキストを取得
+            const answerDiv = div.querySelector("div.markdown") as HTMLDivElement | null;
+            if (!answerDiv) continue;
+            const text = html2markdown(answerDiv);
 
             // 回答生成中ならばループ継続
             if (text !== generatingText) {
@@ -57,6 +75,7 @@ export const chatgptMonitoring = async ({ page }: { page: Page }): Promise<strin
             }
 
             // 回答完了したらループ終了
+            prevConversationTurn = conversationTurn;
             break;
         } catch (error) {
             console.error(error);
