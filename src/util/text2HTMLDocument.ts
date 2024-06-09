316d56b9c675d597f4c53b17755ba3ce2f833a23
ngmtine <ngmfries@gmail.com>
2024-06-09 22:04:18 +0900

    rm: いらんやつ削除

--- src/util/text2HTMLDocument.ts
+++
@@ -1,9 +0,0 @@
-import { JSDOM } from "jsdom";
-
-// 文字列をHTMLDocumentにパース
-export const text2HTMLDocument = (text: string): Document => {
-    const jsdom = new JSDOM();
-    const parser = new jsdom.window.DOMParser();
-    const doc = parser.parseFromString(text, "text/html");
-    return doc;
-};
